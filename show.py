df5dbe1cd04192c0d525af369d85f12eb29dd5bd
Sujal <thakursujal0001@gmail.com>
2025-06-25 15:32:15 +0530

    Improved the display of the game board

--- show.py
+++ show.py
@@ -1,3 +1,7 @@
-def display(arr): 
-    for i in range(len(arr)): 
-        print(arr[i])
+def display(board):
+    print()                              # blank line for spacing
+    for i in range(3):                   # three rows
+        print(f" {board[i][0]} | {board[i][1]} | {board[i][2]} ")
+        if i < 2:
+            print("---+---+---")         # row separator
+    print()
